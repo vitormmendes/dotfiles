c1f1eb9bbe572349bd028f3f6af13bac6f4501ce
Vitor Mendes <vitor.vmendes@gmail.com>
2018-07-21 16:42:09 -0300

    personal adjusts

--- src/os/install/ubuntu/misc_tools.sh
+++ src/os/install/ubuntu/misc_tools.sh
@@ -55,16 +55,3 @@ if ! package_is_installed "dbeaver-ce"; then
         || print_error "dbeaver-ce (resync package index files)"
 
 fi
-
-if ! package_is_installed "code"; then
-
-    sudo add-apt-repository -y "deb https://packages.microsoft.com/repos/vscode stable main"
-
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
-
-    update &> /dev/null \
-        || print_error "dbeaver-ce (resync package index files)"
-
-fi
-
-install_package "Visual Studio Code" "code"
